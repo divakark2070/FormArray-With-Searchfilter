7b74d0e1ba62afd0fe6c3a0484945bee649784b3
Divakar Kesarkar <divakark7020@gmail.com>
2023-04-24 14:46:12 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FormArray_with_Searchfilter';
+export class AppComponent  implements OnInit{
+  title = 'ARRAY';
+  formarry: any
+  signupForm: any;
+
+  constructor() { }
+
+  get hobbyControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      "userData": new FormGroup({
+        "username": new FormControl("", Validators.required),
+        "password": new FormControl("", Validators.required),
+      }),
+      "hobbies": new FormArray([])
+    })
+  }
+
+  submit(data: any) {
+
+    // console.log(data);
+    alert("Data Submitted !!");
+
+  }
+
+  onAddHobby() {
+    let control = new FormControl("", [Validators.required]);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterComponent } from './filter/filter.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    Ng2SearchPipeModule
+
+  ],
+
+  exports:[
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
